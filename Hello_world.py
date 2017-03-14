9ce1e0527a66b2755319eb95496e852eb0e19920
alexpirogovski <pirogovski@gmail.com>
2017-03-14 20:20:35 +0200

    Added parser

--- Hello_world.py
+++ Hello_world.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 import logging
+from optparse import OptionParser
 
 logger = logging.getLogger("Schultz")
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 logger.info("Hello, world!")
 print "Hello, world!"
+
+parser=OptionParser()
+parser_parse_args = parser.parse_args()
+
+
+def read_param(message):
+    print message
+
+read_param(parser_parse_args)
 
 
 
