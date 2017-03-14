2a1123e6a94d1b4be1a4793182415176d9c4edf5
Alex Pirogovski <alexpirogovski@Alexs-MacBook-Pro.local>
2017-03-14 19:47:45 +0200

    My first version

---
+++ Hello_world.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import logging
+
+logger = logging.getLogger("Schultz")
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+logger.info("Hello, world!")
+print "Hello, world!"
+
+
+
+
+
