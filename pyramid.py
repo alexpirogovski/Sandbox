e9e96c811e353114042e3d83d76a6f4d147b77d8
sapienz <sapienz@gmail.com>
2017-03-16 20:02:27 +0200

    modified to support py3

--- pyramid.py
+++ pyramid.py
@@ -6,12 +6,11 @@ layers = int(layers)
 
 
 def print_chars(char, times):
-    for i in range(0,times):
-        print(char), # comma at the end does not print \n
+    for i in range(0, times):
+        print(char, end=" ")  # comma at the end not needed in py3 \n
 
 
-for j in range(0,layers):
-    print_chars(" ",layers-(j+1))
-    print_chars("*", j*2+1)
+for j in range(0, layers):
+    print_chars(" ", layers - (j + 1))
+    print_chars("*", j * 2 + 1)
     print("")
-
