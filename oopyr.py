f1e993ece5cdf26972ac6b4ed63959325113c755
alexpirogovski <pirogovski@gmail.com>
2017-03-18 14:14:51 +0200

    Added comment for end of class definition

---
+++ oopyr.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+class Pyramid:
+    'A class defining a pyramid'
+    def __init__(self, char, layers):
+        self.char = char
+        self.layers = layers
+
+    def print_chars(self, ch, times):
+        for i in range(0, times):
+            print(ch),
+
+    def Draw(self):
+        for j in range(0, self.layers):
+            self.print_chars(" ", self.layers - j - 1)
+            self.print_chars(self.char, j * 2 + 1)
+            print("")
+## End of class definition
+
+
+p = Pyramid('*',10)
+p.Draw()
+
+
+
+
+
+
+
+
+
